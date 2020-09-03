1f996cd5ed67dee6e3f4f1ebd80ff52269027042
Alefe Oliveira <alefe.oliveira@verity.com.br>
2020-09-03 16:13:55 -0300

    create spring project

---
+++ src/main/java/br/com/petx/advertsmicroservice/AdvertsMicroserviceApplication.java
@@ -0,0 +1,13 @@
+package br.com.petx.advertsmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdvertsMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdvertsMicroserviceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/petx/advertsmicroservice/AdvertsMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.petx.advertsmicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdvertsMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
