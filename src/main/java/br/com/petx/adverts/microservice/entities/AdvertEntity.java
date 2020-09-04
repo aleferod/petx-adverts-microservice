5fec20c5b0e2ae9b0fad7e41f8cb0663f822c3f9
Alefe Oliveira <alefe.oliveira@verity.com.br>
2020-09-04 10:48:05 -0300

    Configurando Setup da aplicação

--- src/main/java/br/com/petx/adverts/microservice/entities/AdvertEntity.java
+++ src/main/java/br/com/petx/adverts/microservice/entities/AdvertEntity.java
@@ -1,8 +1,17 @@
 package br.com.petx.adverts.microservice.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 @Entity(name="advert")
 public class AdvertEntity {
+	
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
 
 }
